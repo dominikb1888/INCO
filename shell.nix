7146e07920940838f04ffb4d54839163cd9aa7ac
Dominik Boehler <dominik.boehler@gmx.net>
2022-10-27 16:09:34 +0200

    changes and adaptions Session 3

--- shell.nix
+++ shell.nix
@@ -12,11 +12,12 @@ let
     numpy
     rich
     seaborn
-    # ipywidgets
+    ipywidgets
   ];
 
 in pkgs.mkShell {
   buildInputs = with pkgs; [
+    python311
     pythonEnv
 
     # keep this line if you use bash
