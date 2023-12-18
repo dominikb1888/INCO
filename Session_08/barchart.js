150704df5ea9a155eb2b4832b0db2b313e6c51bb
Dominik Boehler <dominik.boehler@gmx.net>
2023-12-18 11:08:32 +0100

    map with markers, responsiveness still off

--- Session_08/barchart.js
+++ Session_08/barchart.js
@@ -7,55 +7,55 @@ class Barchart {
 
     draw() {
       const chartHolder = d3.select(this.element)
-      const boundingRect = chartHolder.node().getBoundingClientRect(); 
-      this.width = boundingRect.width; 
+      const boundingRect = chartHolder.node().getBoundingClientRect();
+      this.width = boundingRect.width;
       this.height = boundingRect.height;
-    
+
       this.element.innerHTML = '';
-  
-      const svg = chartHolder.append("svg") 
-        .attr("width", this.width) 
-        .attr("height", this.height) 
-      
+
+      const svg = chartHolder.append("svg")
+        .attr("width", this.width)
+        .attr("height", this.height)
+
       this.chart = svg
-        .append("g").classed('chart', true) 
+        .append("g").classed('chart', true)
         // .attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")");
-  
+
       this.createScales();
       this.addAxes();
       this.addBars();
     }
-    
+
     createScales() {
       const maxWinners = d3.max(this.data, d => d.value);
 
       this.yscale = d3.scaleLinear()
         .domain([0, maxWinners])
         .range([this.height, 0]);
-    
+
       this.xscale = d3
         .scaleBand()
         .domain(this.data.map(d=>d.key))
         .range([0, this.width/this.data.length*this.data.length]);
     }
-    
+
     addAxes() {
       const yaxis = d3.axisLeft().scale(this.yscale);
       const xaxis = d3.axisBottom().scale(this.xscale);
 
       this.chart.append("g").attr("id","y-axis")
         .call(yaxis);
-    
+
       this.chart.append("g").attr("id","x-axis")
         .attr('transform','translate(0,' + this.height + ')')
         .call(xaxis)
         .selectAll('text')
         .text('')
     }
-    
+
     addBars() {
       const barWidth = this.width / this.data.length;
-  
+
       this.chart.append("g").selectAll('rect')
         .data(this.data, d => d)
         .join('rect').classed('bar', true)
@@ -65,7 +65,7 @@ class Barchart {
         .attr('x', (d, i) => i * (barWidth) + barWidth/6)
         .attr("stroke-width",2)
         .attr("stroke", "white")
-    
+
      this.chart.append("g").selectAll('foreignObject')
         .data(this.data)
         .join('foreignObject')
@@ -74,9 +74,9 @@ class Barchart {
           .attr("x", function (d,i) { let pos = (i * barWidth) + 3; if (d.prizes_won >= 100) { pos = pos - 5 }; return pos;})
           .attr("y", this.height)
         .append('div')
-          .attr('class', d=>'fib fi-'+d['alpha-2'])        
+          .attr('class', d=>'fib fi-'+d['alpha-2'])
     }
-    
+
     setData(newData) {
       this.data = this.getCount(newData);
       this.draw();
--- Session_08/map.js
+++ Session_08/map.js
@@ -3,45 +3,45 @@ class WorldMap {
       this.element = document.querySelector(opts.element);
       this.data = opts.data;
       this.world = opts.basemap;
-  
+
       this.backgroundColor = "#EAF2FA";
       this.landColor = "#09A573";
       this.landStroke = "#FCF5E9";
       this.markerColor = "#E26F99";
-  
+
       this.chartWidth = 1000;
       this.chartHeight = 500;
-  
+
       this.draw();
     }
-  
+
     draw() {
       const mapHolder = d3.select(this.element);
       this.mymap = mapHolder.append('svg')
         .attr("title", "Map")
         .attr('width', this.chartWidth)
         .attr('height', this.chartHeight);
-  
+
       this.createProjection();
       this.createMap();
       this.updateMarkers(); // Changed to updateMarkers()
     }
-  
+
     createProjection() {
       this.projection = d3.geoMercator()
         .scale([180])
         .center([-60, 50])
         .translate([this.chartWidth / 4, this.chartHeight / 3]);
-  
+
       this.path = d3.geoPath().projection(this.projection);
     }
-  
+
     createMap() {
       this.mymap.append("rect")
         .attr("width", this.chartWidth)
         .attr("height", this.chartHeight)
         .attr('fill', this.backgroundColor);
-  
+
       this.mymap.append('g').selectAll('path')
         .data(this.world.features)
         .join('path')
@@ -50,13 +50,13 @@ class WorldMap {
         .attr('stroke', this.landStroke)
         .attr('stroke-width', 1);
     }
-  
+
     updateMarkers() {
       const circles = this.mymap.select('g').selectAll("circle")
         .data(this.data);
-  
+
       circles.exit().remove();
-  
+
       circles.enter()
         .append("circle")
         .merge(circles)
@@ -67,10 +67,9 @@ class WorldMap {
         .attr("r", 5)
         .style("fill", "red");
     }
-  
+
     setData(newData) {
       this.data = newData;
       this.updateMarkers(); // Call updateMarkers() to update markers when data changes
     }
   }
-  
